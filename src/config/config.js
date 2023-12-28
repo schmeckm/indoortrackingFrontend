8efde6042f15c3c94f2bd5c0c770ffddabec4a5b
schmeckm <markus.schmeckenbecher@gmail.com>
2023-12-28 20:03:21 +0100

    Version 0.9.1

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,5 @@
 const config = {
-    PARETOANYWHERE_URL: "http://35.232.238.172:3001",
+    PARETOANYWHERE_URL: "http://136.244.90.128:3001",
     BACKEND_URL: "http://192.168.1.128:3002/api"
 };
 export default config;
